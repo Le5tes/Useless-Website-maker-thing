5c661825b58d1c0883abbcd963e78e3c510773cd
Tim Williamson <timwilliamson1337@gmail.com>
2017-12-04 18:39:25 +0000

    using forms

--- app.rb
+++ app.rb
@@ -4,11 +4,17 @@ get '/' do
   "Hello World"
 end
 get '/another_one' do
-	"You are a moron"
+	@name = params[:name]
+  "You are a moron #{@name}"
 end
 get '/nice' do
-	"you aren't a moron"
+  "you aren't a moron"
 end
-get '/cat' do
-	erb(:index)
+get '/random_cat' do
+  @name = ["Jebediah", "Bill", "Bob", "Valentina" ].sample
+  erb(:index)
+end
+get '/named_cat' do
+  @name = params[:name]
+  erb(:index)
 end
